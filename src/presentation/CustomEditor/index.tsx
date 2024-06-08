548dc54cc053b7f767bfc26217fbf329597fff4d
maestro.trastane <maestro.trastane@bytedance.com>
2024-06-09 00:27:18 +0700

    fix(ui): remove unused functionalities on rich text editor

--- src/presentation/CustomEditor/index.tsx
+++ src/presentation/CustomEditor/index.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 interface Props {
   onReady: (editor: ClassicEditor) => void;
-  onChange: (_: unknown ,editor: ClassicEditor) => void;
+  onChange: (_: unknown, editor: ClassicEditor) => void;
 }
+
+const toolbarItems = [
+  'undo',
+  'redo',
+  '|',
+  'heading',
+  '|',
+  'bold',
+  'italic',
+  '|',
+  'link',
+  '|',
+  'bulletedList',
+  'numberedList',
+];
 
 const CustomEditor = ({ onReady, onChange }: Props) => {
   return (
-    <CKEditor editor={ClassicEditor} onReady={onReady} onChange={onChange} />
-  )
-}
+    <CKEditor
+      editor={ClassicEditor}
+      onReady={onReady}
+      onChange={onChange}
+      config={{ toolbar: toolbarItems }}
+    />
+  );
+};
 
 export default CustomEditor;
