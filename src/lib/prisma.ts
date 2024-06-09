fa4c70e91856713ebc11403690f1bd3876591862
maestro.trastane <maestro.trastane@bytedance.com>
2024-06-09 22:32:08 +0700

    feat(monitoring): add time logger

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,42 +10,61 @@ class DBClient {
   private static instance: DBClient;
   private constructor() {
     const _prisma = new PrismaClient();
-    const _prismaWithCache = _prisma.$extends(cacheMiddleware).$extends({
-      name: 'admin-action-tracker',
-      model: {
-        log: {
-          /**
-           * This function will capture the data exchange into DB
-           */
-          async capture({
-            action,
-            entity,
-            userId,
-            data,
-            dataId,
-            dataOld,
-          }: {
-            action: string;
-            entity: string;
-            userId: string;
-            data: string;
-            dataId: string;
-            dataOld: string;
-          }) {
-            await _prisma.log.create({
-              data: {
-                action,
-                entity,
-                user_id: userId,
-                data,
-                data_old: dataOld,
-                data_id: dataId
-              }
-            })
+    const _prismaWithCache = _prisma
+      .$extends(cacheMiddleware)
+      .$extends({
+        name: 'admin-action-tracker',
+        model: {
+          log: {
+            /**
+             * This function will capture the data exchange into DB
+             */
+            async capture({
+              action,
+              entity,
+              userId,
+              data,
+              dataId,
+              dataOld,
+            }: {
+              action: string;
+              entity: string;
+              userId: string;
+              data: string;
+              dataId: string;
+              dataOld: string;
+            }) {
+              await _prisma.log.create({
+                data: {
+                  action,
+                  entity,
+                  user_id: userId,
+                  data,
+                  data_old: dataOld,
+                  data_id: dataId,
+                },
+              });
+            },
           },
         },
-      },
-    });
+      })
+      .$extends({
+        name: 'time-logger',
+        query: {
+          async $allOperations({ model, args, operation, query }) {
+            const before = Date.now();
+            const result = await query(args);
+            const after = Date.now();
+
+            console.log(
+              '\x1b[36m',
+              `[time-logger] Query ${operation} of ${model} took -> ${after - before}ms`,
+              '\x1b[0m'
+            );
+            return result;
+          },
+        },
+      });
 
     this.prisma = _prismaWithCache;
   }
